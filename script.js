ecf6fd7146469270b9baaa59646be433fc3ae39b
Your Name <you@example.com>
2023-11-14 10:15:17 +0100

    added date selector to NASA api

--- script.js
+++ script.js
@@ -6,24 +6,40 @@ const fetchUrl = async (url) => {
     return response.json()
 }
 
+const inputComponent = () => `
+    <div class="input">
+        <input type="date" name="date">
+    </div>
+`
+
 const apodComponent = (apodData) => `
-    <h2>${apodData.title}</h2>
-    <h3>${apodData.date}</h3>
-    <p>${apodData.explanation}</p>
-    <img src=${apodData.url}>
+    <div class="apodDiv">
+        <h2>${apodData.title}</h2>
+        <h3>${apodData.date}</h3>
+        <p>${apodData.explanation}</p>
+        <img src=${apodData.url}>
+    </div>
 `
 
 const init = async () => {
     const data = await fetchUrl(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
-    
-    const arrayData = await fetchUrl(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&count=5`)
+
+    /* const arrayData = await fetchUrl(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&count=5`)
     console.log(arrayData)
 
     const dataByDate = await fetchUrl(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=2000-07-14`)
-    console.log(dataByDate)
+    console.log(dataByDate) */
 
     //rootElement.innerHTML = `<h2>${data.title}</h2>`
+    rootElement.insertAdjacentHTML("beforeend", inputComponent())
     rootElement.insertAdjacentHTML("beforeend", apodComponent(data))
+
+    const apodDiv = document.querySelector(".apodDiv")
+    const inputElement = document.querySelector("input")
+    inputElement.addEventListener("input", async () => {
+        const inputDate = await fetchUrl(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${inputElement.value}`)
+        apodDiv.innerHTML = apodComponent(inputDate)
+    })
 }
 
 init()
